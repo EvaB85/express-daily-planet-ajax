c8ef807ae249d8a8fe155bbdc499a9fc931dc754
Eva Barrett <Evabarrett23@Gmail.com>
2018-06-14 19:45:37 -0700

    making progress on two links

--- index.js
+++ index.js
@@ -1,52 +1,62 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var db = require('./models');
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/static'));
 
-var articles = [
-  { title: 'Bernie! Bernie!', body: '#feelthebern' },
-  { title: 'Trump for change!', body: 'Make America Great Again' },
-  { title: 'Brian Hague founds the Daily Planet', body: 'Wow! Amazing! Such good news!' }
-];
+// var articles = [
+//   { title: 'Bernie! Bernie!', body: '#feelthebern' },
+//   { title: 'Trump for change!', body: 'Make America Great Again' },
+//   { title: 'Brian Hague founds the Daily Planet', body: 'Wow! Amazing! Such good news!' }
+// ];
 
 // GET / - gets the main site index page
 app.get('/', function(req, res) {
   res.render('index');
 });
-// GET /about - gets the main site about page
+
+// GET /about - gets the main site about page (get ALL)
 app.get('/about', function(req, res) {
-  res.render('about');
+  db.article.findAll().then(function(data) {
+  res.render(about);
+  });
 });
 
-// GET /articles - gets full articles list
+// GET /articles - gets full articles list (db.article  find ALL)
 app.get('/articles', function(req, res) {
-  // TODO: Add db access code here.
-  res.render('articles/index', { articles: articles });
-});
-
-// GET /articles/:index - gets a specific article
-app.get('/articles/:index', function(req, res) {
-  var index = parseInt(req.params.index);
-  if (index < articles.length && index >= 0) {
-    // TODO: Add db access code here.
-    res.render('articles/show', { article: articles[req.params.index] });
-  } else {
-    res.send('Error');
-  }
+    db.article.findAll().then(function(data) {
+      console.log(data);
+      res.render('articles/index', { articles: data});
+  });
 });
 
 // GET /articles/new - returns form for new article
 app.get('/articles/new', function(req, res) {
   res.render('articles/new');
 });
+
+// GET /articles/:index - gets a specific article
+app.get('/articles/:index', function(req, res) {
+  var index = parseInt(req.params.index);
+  db.article.findAll().then(function(articles) {
+    res.render('articles/show', { article: articles[req.params.index] });
+  })
+});
+
 // POST /articles - create a new article from form data
 app.post('/articles', function(req, res) {
-  // TODO: Add db access code here.
-  res.redirect('/articles');
-});
+  db.article.create({
+    title: req.body.title,
+    body: req.body.body
+    }).then(function(data) {
+      console.log(data);
+      res.redirect('/articles');
+    });
+  });
+
 
 app.listen(3000, function() {
     console.log("You're listening to the smooth sounds of port 3000 in the morning");
